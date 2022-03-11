755d9bb54da0fd852370dc039d6a7f38f59fa74b
Natàlia Sànchez <nsanchez.martinez@atsistemas.com>
2022-03-11 18:00:16 +0100

    Ordre als fitxers i treiem jquery

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,8 @@
 const path = require('path')
 
 module.exports = {
+    entry: './_src/js/*.js',
+
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'assets/dist'),
